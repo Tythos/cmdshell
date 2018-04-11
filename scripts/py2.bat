044593b96b8223226649ae14398b995a10984e61
Brian Kirkpatrick <code@tythos.net>
2018-04-11 09:55:58 -0700

    Adding streamlined Python environment handlers

---
+++ scripts/py2.bat
@@ -0,0 +1,10 @@
+@echo off
+rem Activates a Python environment. To customize, specify PYVER value.
+if not "%PYTHONPATH%"=="" (
+    call unpy
+)
+set PYVER=Python27
+set PYPATH=%USERPROFILE%\AppData\Local\Programs\Python\%PYVER%
+set PATH=%PATH%;%PYPATH%;%PYPATH%\Scripts
+set PYTHONPATH=%USERPROFILE%\Projects
+prompt [%PYVER%] $P$G
